1df29c89f17266026324c9692c0afb37a9a953d9
Animesh sahu <animesh.sahu@xpo.com>
2024-11-02 16:44:18 +0530

    made changes

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+  console.log('event');
   // CREATE
   if (eventType === "user.created") {
     console.log('event',evt);
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (isPublicRoute(request)) {
+  if (!isPublicRoute(request)) {
     await auth.protect()
   }
 })
